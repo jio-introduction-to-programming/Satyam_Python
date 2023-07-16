9dcf4649b974e28ac9e282c755482cb44b07daac
Satyam Chaurasiya <83088896+satyamsc0@users.noreply.github.com>
2023-07-16 11:27:23 +0530

    new changes

--- classwork.py
+++ classwork.py
@@ -43,8 +43,9 @@ x = 3.141516
 formatted = '{:.2f}'.format(x)
 print(formatted)
 
-# formatted = f'{.2f}'
-# print(formatted)
+y = 1.0
+formatted = f'{y:.2f}'
+print(formatted)
 
 birthdate = input("Enter your upcoming birthdate in dd/mm/yyyy format: ")
 birthdate = datetime.strptime(birthdate, '%d/%m/%y')
@@ -123,3 +124,120 @@ for num in numbers:
         continue
     print(num)
 
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    if num % 2 == 0:
+        break
+    print(num)
+else:
+    print("All numbers are even")
+
+##########################################################
+
+lst = []
+lst_all_dtypes = [1, 'a', 0.33]
+lst_all_dtypes
+
+grid = [[0, 1, 0],
+        [1, 1, 0],
+        [0, 0, 1]]
+
+grid
+
+odds = [x for x in range(1,10,2)]
+odds
+
+print(*[x for x in range(0,10) if x%2 == 0])
+
+a = [[0]*5]*5
+a[0][0] = 2
+a
+
+import random
+
+[random.randint(1,100) for _ in range(5)]
+
+[random.random() for _ in range(2)]
+
+lst_sample = random.sample(range(1,100), 5)
+lst_sample
+
+my_list = [1, 2, 3, 4, 5]
+random.shuffle(my_list)
+my_list
+
+del my_list[2]
+my_list
+my_list[-4]
+# list slicing mylist[start:stop:step]
+new_list = my_list[0:4:2]
+# new_list.clear()
+new_list
+
+c = my_list.pop()
+c
+
+id(my_list[0]) == id(new_list[0])
+my_list.index(10)
+my_list.pop(-1)
+
+dict_a = {True: a}
+dict_a
+dir(dict_a)
+
+keys = ['a', 'b', 'c']
+my_dict = {k:v for k,v in enumerate(keys)}
+my_dict
+default_price = 0.99
+eg_dict = dict.fromkeys(keys, default_price)
+eg_dict
+price_list = [1, 3, 5]
+eg2_dict = dict.fromkeys(keys, price_list) # cannot pass a list of values, other
+eg2_dict
+
+print(my_dict.get('d', 'default'))
+
+my_dict.items() # returns a list - items(), keys(), values()
+
+{}.pop('key','a')
+
+a_dict = {1 : 'a',
+          2 : 'b'}
+
+b_dict = dict(a_dict)
+b_dict
+last_inserted_item = b_dict.popitem()
+last_inserted_item
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+g = []
+h = []
+id(g) == id(h)
+
+a = [1,2,3]
+b = a[:]
+
+print(id(a), id(b), sep='\n')
+
+b[0] = 4
+
+a[1] = 5
+
+print(a, b, sep='\n')
+
+id(a[1]) == id(b[1])
+
+import copy
+
+orig_list = [1, 2, 3, 4]
+new_list = copy.deepcopy(orig_list)
+
+id(orig_list[1]) == id(new_list[1])
