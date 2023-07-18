7dd724533bd14acd73324812a900479eb2ffad30
Satyam Chaurasiya <83088896+satyamsc0@users.noreply.github.com>
2023-07-18 21:28:31 +0530

    Assignment 4 completed 18 july 2023

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -50,3 +50,41 @@ def insertionSort(arr):
 
 insertionSort([3,2,4,1,5,3])
 
+
+# 5. Write a function which takes a list as an input and returns the following statistics: 
+#     1. mean/average
+#     2. mode
+
+def calc_stats(lst):
+    mean = sum(lst)/len(lst)
+    nums_dict = {}
+    for num in lst:
+        if num in nums_dict:
+            nums_dict[num] += 1
+        else:
+            nums_dict[num] = 1
+    
+    mode_count = max(nums_dict.values())
+    mode = [key for key in nums_dict.keys() if nums_dict[key] == mode_count]
+
+    return f'mean is: {mean}, \nmode is: {mode}'
+
+print(calc_stats([1,1,1,2,2,2,3,8,12]))
+
+
+# 6. 
+# Step-1 Read a sequence of numbers as an input from user. 
+# Step-2 Read another integer k from the user 
+# Step-3 Ask user to input two number start and end, k times 
+# Step-4 Return the sum of the array from start -> end as a result in the k-sized list 
+
+# arr = 1 5 -6 10 -34 19 8 6 , k = 3
+arr = list(map(int, input('Enter space separated integers (minimum 5 numbers): ').split()))
+k = int(input('Enter the number of times you want the sum: '))
+lst = []
+for i in range(k):
+    indexes = list(map(int, input('Enter the start and end index values (space separated) to get the sum from the list: ').split()))
+    sum_start_to_end_index = add(arr[indexes[0]: indexes[1]+1])
+    lst.append(sum_start_to_end_index)
+print(lst)
+
